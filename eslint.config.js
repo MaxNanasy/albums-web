5f013972b0df585c01300c1ec170f96fa1364478
Max Nanasy <max.nanasy@gmail.com>
2026-04-12 14:50:15 -0700

    [WIP] Tweak eslint stuff

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,27 @@
-import tsParser from '@typescript-eslint/parser';
-import tsPlugin from '@typescript-eslint/eslint-plugin';
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
-export default [
+export default tseslint.config(
+  // js.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  // ...tseslint.configs.stylisticTypeChecked,
   {
-    files: ['src/**/*.js', 'tests/**/*.js'],
+    files: ['*.js', 'src/**/*.js', 'tests/**/*.js'],
     languageOptions: {
-      parser: tsParser,
+      // parser: tsParser,
       parserOptions: {
         project: ['./tsconfig.json', './tests/tsconfig.json'],
         tsconfigRootDir: import.meta.dirname,
-        ecmaVersion: 'latest',
-        sourceType: 'module',
+        // ecmaVersion: 'latest',
+        // sourceType: 'module',
       },
     },
-    plugins: {
-      '@typescript-eslint': tsPlugin,
-    },
-    rules: {
-      '@typescript-eslint/no-unsafe-assignment': 'error',
-    },
+    // plugins: {
+    //   '@typescript-eslint': tsPlugin,
+    // },
+    // rules: {
+    //   '@typescript-eslint/no-unsafe-assignment': 'error',
+    //   '@typescript-eslint/no-unsafe-type-assertion': 'error',
+    // },
   },
-];
+);
